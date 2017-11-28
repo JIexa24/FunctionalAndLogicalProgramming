98b72ef79e3fa960636fbfb89b7675bfa56e5617
Alexey R <lexa260011@ngs.ru>
2017-11-27 23:45:40 -0100

    Я не знаю, почему оно сработало.Но оно сработало

---
+++ rgz/rgz.pl
@@ -0,0 +1,5 @@
+insert(Element, [], [Element]):-!.
+insert(Element, [Head|Tail], [Element, Head|Tail]):-
+  Element < Head, !.
+insert(Element, [Head|Tail], [Head|InsertTail]):-
+  insert(Element, Tail, InsertTail).
