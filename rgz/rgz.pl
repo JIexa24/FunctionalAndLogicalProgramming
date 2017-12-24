11f696ea7086869d7064b36617c4907dcd0d14ea
Alexey R <lexa260011@ngs.ru>
2017-12-25 01:26:17 +0700

    rgz?

--- rgz/rgz.pl
+++ rgz/rgz.pl
@@ -3,3 +3,44 @@ insert(Element, [Head|Tail], [Element, Head|Tail]):-
   Element < Head, !.
 insert(Element, [Head|Tail], [Head|InsertTail]):-
   insert(Element, Tail, InsertTail).
+
+run:-
+  consult('basesrc.dat'),
+  menu.
+
+menu:-
+      repeat,
+      write('-----------------------'),nl,nl,
+      write('БАЗА ДАННЫХ: Итоги сессии студентов'),nl,nl,
+      write('1–Просмотреть базу данных.'),nl,
+      write('2–Добавить новые итоги сессии в базу данных. '),nl,
+      write('--------------------------------'),nl,
+      write('Выберите нужный пункт меню: [1-6] '),
+      read(X),
+      X<7,
+      process(X),
+      X=6,!.
+
+process(1):-view_base.
+process(2):-add_base.
+
+process(6):-retractall(student/3),!.
+
+view_base:-
+  student(Group,Name,Marks),
+  write('Group: '), write(Group),nl,
+  write('Name: '), write(Name),nl,
+  write('Marks: '), write(Marks),nl,
+  write('-------------------------------'),nl.
+
+add_base:-
+  write('Add to base:'),nl,nl,
+  repeat,
+  write('Name: '),
+  read(Name),
+  write('Group: '),
+  read(Group),
+  write('Marks: '),
+  read(Marks),
+  assert(student(Group,Name,Marks)),
+  quest,!.
