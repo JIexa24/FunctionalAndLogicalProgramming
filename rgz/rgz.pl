bc4dde5740a2441fc891dae39dfc4e9427cbf350
Alexey R <lexa260011@ngs.ru>
2018-01-09 09:29:05 +0700

    -

--- rgz/rgz.pl
+++ rgz/rgz.pl
@@ -14,18 +14,32 @@ menu:-
       write('-----------------------'),nl,nl,
       write('БАЗА ДАННЫХ: Итоги сессии студентов'),nl,nl,
       write('1–Просмотреть базу данных.'),nl,
-      write('2–Добавить новые итоги сессии в базу данных. '),nl,
+      write('2–Добавить новую запись в базу данных. '),nl,
+      write('3–Удалить запись из базы данных. '),nl,
+      write('4–Загрузить базу из файла. '),nl,
+      write('5–Сохранить базу в файл. '),nl,
       write('--------------------------------'),nl,
-      write('Выберите нужный пункт меню: [1-6] '),
+      write('Выберите нужный пункт меню: [1-6] (7 - выход) '),
       read(X),
-      X<7,
+      X<8,
       process(X),
-      X=6,!.
+      X=7,!.
 
 process(1):-view_base.
 process(2):-add_base.
+process(3):-remove_base.
+process(4):-load_base.
+process(5):-save_base.
+process(7):-retractall(student/3),!.
 
-process(6):-retractall(student/3),!.
+load_base:-
+  consult('basesrc.dat'),!.
+
+save_base:-
+  tell('basedst.dat'),
+  listing(airport),
+  told,
+  write('Текстовый файл базы данных(basedst.dat) сохранен!'),nl.
 
 view_base:-
   student(Group,Name,Marks),
@@ -46,7 +60,12 @@ add_base:-
   assertz(student(Group,Name,Marks)),
   again,!.
 
-
+remove_base:-
+  write('Удаление записи'),nl,nl,
+  write('Введите Имя для удаления: '),
+  read(Name),
+  retract(student(_,Name,_)),
+  write('Запись удалена!'),nl,nl.
 
 again :-
   write('Ввести еще одно ? y/n '),
